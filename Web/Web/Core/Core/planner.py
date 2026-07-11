9781fa0eebc822efdef575f0088aa88e3317a04b
Azazel-develop <dariopelgesenker@gmail.com>
2026-07-11 09:09:05 +0200

    Add Planner class with create_plan method

---
+++ Web/Web/Core/Core/planner.py
@@ -0,0 +1,16 @@
+class Planner:
+
+
+    def create_plan(self,goal):
+
+        return [
+
+            "Understand request",
+
+            "Create steps",
+
+            "Execute safely",
+
+            "Report result"
+
+        ]
